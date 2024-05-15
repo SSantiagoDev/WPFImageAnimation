6c662976ef534da3043f2d53bc29cd142c555d5d
Steeve Santiago <HCC756@Belgrid.net>
2024-05-15 09:35:57 +0200

    Add project files.

---
+++ ImageAnimation/MainWindowVM.cs
@@ -0,0 +1,46 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+
+namespace ImageAnimation
+{
+    public class MainWindowVM: BindableBase
+    {
+        #region  Properties
+
+        private string _status;
+        public string Status
+        {
+            get { return _status; }
+            set { SetProperty(ref _status, value); }
+        }
+
+        #endregion
+
+        #region Commands
+
+        private DelegateCommand _startCommand;
+        public DelegateCommand StartCommand =>
+            _startCommand ?? (_startCommand = new DelegateCommand(Start));
+
+        private DelegateCommand _stopCommand;
+        public DelegateCommand StopCommand =>
+            _stopCommand ?? (_stopCommand = new DelegateCommand(Stop));
+
+        #endregion
+
+        public MainWindowVM()
+        {
+            this.Status = "Not started yet";
+        }
+
+        private void Start()
+        {
+            this.Status = "Running";
+        }
+
+        private void Stop()
+        {
+            this.Status = "Stopped";
+        }
+    }
+}
